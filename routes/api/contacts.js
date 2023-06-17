a87dd8e1bc584c64b5eb50eedf4e21811b69d53c
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-06-17 13:31:14 +0200

    make controllers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,51 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
+const API = require('../../models/contacts');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await API.listContacts();
+  res.json({ 
+    status: 200,
+    contacts 
+  });
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const contact = await API.getContactById(contactId);
+  
+  if(!contact) {
+    res.json({ 
+      status: 404,
+      message: 'Not found'
+      })
+      
+  } else {
+    res.json({ 
+      status: 200,
+      message: 'Successful request of contact by id',
+      contact
+      })
+  }
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const body = req.body;
+  const newContact = await API.addContact(body);
+
+  if(!newContact) {
+    res.json({
+      status: 400,
+      message: "missing required name field"
+    })
+  } else {
+    res.json({ 
+      status: 201,
+      newContact
+    })
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
