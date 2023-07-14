c65d10ba1ab0757da5bf14e62c00d0aa53e1107e
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-07-14 10:36:03 +0200

    rename

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, login, getMe, logout } = require('../../controllers/authController');
+const { signup, login, getMe, logout } = require('../../controllers/userController');
 const { checkSignupUserData, protect } = require('../../middlewares/userMiddlewares');
 
 router.post('/register', checkSignupUserData, signup);
